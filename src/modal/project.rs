71d1d01f7ff466656467e4c22db9d11890a537d6
zrll <zhengruilin0@126.com>
2026-03-02 02:59:07 +0800

    feat: refactor project saving functionality into LedMakerProject struct

--- src/modal/project.rs
+++ src/modal/project.rs
@@ -1,12 +1,14 @@
 use std::io;
 use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
+use gpui::Context;
+use rfd::{AsyncFileDialog, MessageDialog};
 use crate::modal::AppError;
+use crate::modal::app_state::AppState;
 
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct LedMakerProject {
     pub name: String,
-    pub font_path: String,
     pub frames: Vec<Frame>,
 }
 
@@ -29,5 +31,54 @@ impl LedMakerProject {
         std::fs::write(path, toml_string)?;
         
         Ok(())
+    }
+
+    pub fn save_project<T: 'static>(&self, cx: &mut Context<T>) {
+        println!("Saving project: {}", self.name);
+        let project = self.clone();
+        let file_path = cx.global::<AppState>().file_path.clone();
+
+        cx.spawn(
+            move |_: gpui::WeakEntity<T>, cx: &mut gpui::AsyncApp| {
+                let cx = cx.clone();
+                async move {
+                    let path = if let Some(path) = file_path {
+                        path
+                    } else {
+                        let file = AsyncFileDialog::new()
+                            .add_filter("project file", &["ledm", "toml"])
+                            .add_filter("all files", &["*"])
+                            .set_directory(".")
+                            .save_file()
+                            .await;
+                        let Some(file) = file else {
+                            return;
+                        };
+                        file.path().to_path_buf()
+                    };
+
+                    let project_for_state = project.clone();
+                    let path_for_state = path.clone();
+                    let _ = cx.update_global::<AppState, _>(|app_state, _| {
+                        app_state.file_path = Some(path_for_state);
+                        app_state.current_project = project_for_state;
+                    });
+
+                    match project.save(&path) {
+                        Ok(_) => println!("Project saved successfully."),
+                        Err(err) => {
+                            println!("Error saving project: {}", err);
+                            MessageDialog::new()
+                                .set_title("Error")
+                                .set_description(format!("Failed to save project:\n{}", err))
+                                .set_buttons(rfd::MessageButtons::Ok)
+                                .set_level(rfd::MessageLevel::Error)
+                                .show();
+                        }
+                    }
+                }
+            },
+        )
+        .detach();
     }
 }
